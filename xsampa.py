d39382bdc098d52652260eb2914642cf4d044655
Jeremy Freudberg <jeremyfreudberg@gmail.com>
2016-04-03 18:49:45 -0400

    Overhaul, functions, docstrings, etc.

--- xsampa.py
+++ xsampa.py
@@ -1,96 +1,61 @@
+### REQURIES PYTHON 3 FOR UNICODE SUPPORT!!! ###
+### YOU REALLY SHOULD BE ABANDON 2.x BY NOW! ###
+
 import sys
-from tkinter import *
 import pyperclip
+import json
+from tkinter import *
 
-# unicode!!!
-correspondence = {'m':'m',
-         'F':'ɱ',
-         'n':'n',
-         'N':'ŋ',
-         'p':'p',
-         'b':'b',
-         't':'t',
-         'd':'d',
-         'k':'k',
-         'g':'g',
-         'q':'q',
-         '?':'ʔ',
-         'f':'f',
-         'v':'v',
-         'T':'θ',
-         'D':'ð',
-         's':'s',
-         'z':'z',
-         'S':'ʃ',
-         'Z':'ʒ',
-         'x':'x',
-         'G':'ɣ',
-         'h':'h',
-         'r\\':'ɹ',
-         'j':'j',
-         'r':'r',
-         '4':'ɾ',
-         'l':'l',
-         'W':'ʍ',
-         'w':'w',      
-         'i':'i',
-         '1':'ɨ',
-         '}':'ʉ',
-         'u':'u',
-         'I':'ɪ',
-         'U':'ʊ',
-         'e':'e',
-         'o':'o',
-         '@':'ə',
-         'E':'ɛ',
-         '3':'ɜ',
-         'V':'ʌ',
-         'O':'ɔ',
-         '{':'æ',
-         '6':'ɐ',
-         'a':'a',
-         'A':'ɑ',
-         'Q':'ɒ'}
-
-# The only multichar symbol is 'r\'
+import data as x
 
 def translate(s):
+    """
+    Loop through the string to convert each character/token
+    Halt (and return an error string) if input is invalid
+
+    Certain phones in X-SAMPA are represented by multiple characters!
+    """
     result = ''
-    for i in range(len(s)):
-        if s[i] != 'r' and s[i] != '\\':
+    for i,current_char in enumerate(s):
+        if current_char != 'r' and current_char != '\\':
             try:
-                result += correspondence[s[i]]
+                result += x.correspondence[current_char]
             except KeyError:
                 result = "I don't think that was valid X-SAMPA (for English, anyway)."
                 break
-        elif s[i] == '\\':
-            pass
-        else: # s[i] = 'r'
+        elif current_char == '\\':
+            continue
+        else: # current_char = 'r'
             try:         
                 if s[i+1] == '\\':
-                    result += correspondence['r\\']
+                    result += x.correspondence['r\\']
                 else:
-                    result += correspondence['r']
+                    result += x.correspondence['r']
             except IndexError:
-                result += correspondence['r']
-                
+                result += x.correspondence['r']                
     return result
 
-try:
-    translation = (translate(sys.argv[1]))
-except:
-    translation = 'No text entered'
+
 
 def copy_to_clipboard():
+    """ Dummy function needed for Tkinter button """
     pyperclip.copy(translation)
-    
-root = Tk()
-root.wm_title('Result')
-e = Entry(root)
-e.insert(0,translation)
-e.pack()
-b = Button(root, text="Copy to clipboard", command=copy_to_clipboard)
-b.pack()
-root.mainloop()
-            
-    
+  
+
+def main():
+    """ Grab the input text, process, and show result in GUI """
+    global translation
+    try:
+        translation = (translate(sys.argv[1]))
+    except IndexError:
+        translation = 'No text entered'
+    root = Tk()
+    root.wm_title('Result')
+    e = Entry(root)
+    e.insert(0,translation)
+    e.pack()
+    b = Button(root, text="Copy to clipboard", command=copy_to_clipboard)
+    b.pack()
+    root.mainloop()
+
+main()
